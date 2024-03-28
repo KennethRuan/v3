c92f2d0220c18d6b89091950fe4d57621816c270
Kenneth Ruan <kennethjruan@gmail.com>
2024-03-27 23:46:24 -0400

    Test 7

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -10,37 +10,39 @@ export const anek = Anek_Latin({ subsets: ["latin"] });
 
 export default function Hero() {
   return (
-    <div className={cn("relative w-full h-screen bg-creme", anek.className)}>
-      <div className="absolute w-full h-full top-0 left-0">
-        <div className="relative w-full h-full lg:w-fit lg:ml-[20%] items-center lg:items-start justify-center flex flex-col text-dusk">
-          <div className="w-fit">
-            <div>
-              <div className="font-semibold text-2xl lg:text-4xl">
-                {"hey i'm kenneth."}
-              </div>
-              <div className="font-normal text-lg lg:text-2xl">
-                {"i'm a software engineering student @ uw."}
+    <div className={cn("relative w-full h-screen", anek.className)}>
+      <FullscreenComponent>
+        <div className="w-full h-full bg-creme">
+          <div className="absolute w-full h-full top-0 left-0">
+            <div className="relative w-full h-full lg:w-fit lg:ml-[20%] items-center lg:items-start justify-center flex flex-col text-dusk">
+              <div className="w-fit">
+                <div>
+                  <div className="font-semibold text-2xl lg:text-4xl">
+                    {"hey i'm kenneth."}
+                  </div>
+                  <div className="font-normal text-lg lg:text-2xl">
+                    {"i'm a software engineering student @ uw."}
+                  </div>
+                </div>
+                <div className="font-normal text-lg lg:text-2xl">
+                  {"trying to be a sponge!"}
+                </div>
+                <div className="h-4" />
+                <div className="text-lg lg:text-2xl">currently...</div>
+                <ul className="list-disc list-inside text-lg lg:text-2xl">
+                  <li>building a music distribution platform</li>
+                  <li>learning videography</li>
+                  <li>trying to run a half marathon</li>
+                </ul>
               </div>
             </div>
-            <div className="font-normal text-lg lg:text-2xl">
-              {"trying to be a sponge!"}
-            </div>
-            <div className="h-4" />
-            <div className="text-lg lg:text-2xl">currently...</div>
-            <ul className="list-disc list-inside text-lg lg:text-2xl">
-              <li>building a music distribution platform</li>
-              <li>learning videography</li>
-              <li>trying to run a half marathon</li>
-            </ul>
+          </div>
+
+          <div className="relative z-[1] w-full h-full">
+            <Scene />
           </div>
         </div>
-      </div>
-
-      <div className="relative z-[1] w-full h-full">
-        <FullscreenComponent>
-          <Scene />
-        </FullscreenComponent>
-      </div>
+      </FullscreenComponent>
     </div>
   );
 }
--- src/components/ui/fullscreen.tsx
+++ src/components/ui/fullscreen.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/utils";
 import React, { useEffect, useState } from "react";
 import { useFullscreenContext } from "../providers/fullscreen-provider";
+import { createPortal } from "react-dom";
 
 type FullscreenComponentProps = {
   children: React.ReactNode;
@@ -14,6 +15,7 @@ const FullscreenComponent = ({ children }: FullscreenComponentProps) => {
     "fixed top-0 left-0 w-screen h-screen z-[9999] overflow-hidden";
 
   useEffect(() => {
+    console.log("doc body", document.body);
     document.body.style.overflow = isFullscreen ? "hidden" : "auto";
   }, [isFullscreen]);
 
@@ -50,7 +52,7 @@ const FullscreenComponent = ({ children }: FullscreenComponentProps) => {
   return (
     <div className="relative w-full h-full">
       <div className={`w-full h-full ${isFullscreen ? fullscreenStyle : ""}`}>
-        {children}
+        {isFullscreen ? createPortal(children, document.body) : <>{children}</>}
       </div>
       <button
         onClick={toggleFullscreen}
